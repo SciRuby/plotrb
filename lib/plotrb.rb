9a7b419a08b028da15e6bb92b6a20b53b503d64f
Daniel Mendler <mail@daniel-mendler.de>
2014-08-01 14:39:31 +0200

    iruby compatibility finished
    vis.show plots the thing

--- lib/plotrb.rb
+++ lib/plotrb.rb
@@ -23,3 +23,21 @@ class Object
   include ::Plotrb::Kernel
 
 end
+
+if defined? IRuby
+  IRuby.display(IRuby.javascript(%{
+    require.config({paths: {
+       d3: 'http://d3js.org/d3.v3.min',
+       d3_geo: 'http://trifacta.github.io/vega/lib/d3.geo.projection.min',
+       topojson: 'http://trifacta.github.io/vega/lib/topojson',
+       vg: 'http://trifacta.github.io/vega/vega.min'
+    }});
+    require(['d3', 'vg', 'd3_geo', 'topojson'], function(d3, vg, d3_geo, topojson) {
+        window.d3 = d3;
+        window.vg = vg;
+        var event = document.createEvent('HTMLEvents');
+        event.initEvent('load_plotrb',false,false);
+        window.dispatchEvent(event);
+    });
+}))
+end
